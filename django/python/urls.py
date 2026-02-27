eb7229b93e29b235a0398fcb4d2c554540544f63
Riya Singh <161211422+riya0227@users.noreply.github.com>
2026-02-27 19:44:25 +0530

    Update urls.py

--- django/python/urls.py
+++ django/python/urls.py
@@ -1,12 +1,16 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
-]
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('upload/', views.upload_report, name='upload_report'),
+    path('reports/', views.report_list, name='report_list'),
+    path('delete/<int:report_id>/', views.delete_report, name='delete_report'),
+    path('edit/<int:report_id>/', views.edit_report, name='edit_report'),
+    path("report-detail/", views.report_detail, name="report_detail"),
+    path("gap-analysis/", views.gap_analysis, name="gap_analysis"),
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
